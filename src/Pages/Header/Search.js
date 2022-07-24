0cdd36a0c65744a64557395ac970b50a20c4f1a1
Md Asgor Office <asgor.ice@gmail.com>
2022-07-24 17:19:58 +0600

    search and navbar design

--- src/Pages/Header/Search.js
+++ src/Pages/Header/Search.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Search = () => {
+    window.addEventListener("scroll", function () {
+        const search = document.querySelector(".search")
+        search.classList.toggle("active", window.scrollY > 100)
+    })
+
     return (
         <>
-            <h1>Search</h1>
+            <section className="search">
+                <div className="container c_flex">
+                    <div className="logo width">
+                        <img src="" alt="" />
+                        <span><b>E-Commerce</b></span>
+                    </div>
+
+                    <div className="search-box f_flex">
+                        <i className="fa fa-search"></i>
+                        <input type="text" placeholder='Search' />
+                        <span>All Category</span>
+                    </div>
+
+                    <div className="icon f_flex width">
+                        <i className="fa fa-user icon_circle"></i>
+                        <div className="cart">
+                            <Link to='/cart'>
+                                <i className="fa fa-shopping-bag icon-circle"></i>
+                                <span>0</span>
+                            </Link>
+                        </div>
+                    </div>
+
+                </div>
+            </section>
         </>
     );
 };
